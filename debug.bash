895245456a088dcd6f8c2f1fdc90f48753893d3b
Altan Orhon <altan@uw.edu>
2024-06-11 17:51:30 -0700

    wip

--- debug.bash
+++ debug.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 function __dhcp_debug_setx_output() {
-    if [[ -n "${1:-}" ]]; then
-        exec {BASH_XTRACEFD}>>"$1"
+    if [ -n "${1:-}" ]; then
+        eval 'exec {BASH_XTRACEFD}>>"$1"'
         PS4=' \D{%Y-%m-%d}T\T [$BASH_SOURCE:$LINENO] '
         set -x
     else
@@ -11,48 +12,26 @@ function __dhcp_debug_setx_output() {
 }
 
 function __dhcp_debug_log_stdout() {
-  exec &> >(ts '%Y-%m-%dT%H:%M:%S' | tee -a "${__log_stdout_path:-out.log}")
+  exec &> >(ts '%Y-%m-%dT%H:%M:%S' | tee -a "$@")
 }
 
 function __dhcp_debug_log_stderr() {
-  exec &> >(ts '%Y-%m-%dT%H:%M:%S' | tee -a "${__log_stderr_path:-out.err}")
+  exec &> >(ts '%Y-%m-%dT%H:%M:%S' | tee -a "$@" >&2)
 }
 
-
-# appends a command to a trap
-#
-# - 1st arg:  code to add
-# - remaining args:  names of traps to modify
-#
-# set the trace attribute for the above function.  this is
-# required to modify DEBUG or RETURN traps because functions don't
-# inherit them unless the trace attribute is set
-
-function __dhcp_debug_trap__add () {
-    local handler=$(trap -p "$2")
-    handler=${handler/trap -- \'/}    # /- Strip `trap '...' SIGNAL` -> ...
-    handler=${handler%\'*}            # \-
-    handler=${handler//\'\\\'\'/\'}   # <- Unquote quoted quotes ('\'')
-    trap "${handler} $1;" "$2"
+function __dhcp_debug_trap_add() {
+    local __dhcp_debug_trap_add_handler=$(trap -p "$2")
+    __dhcp_debug_trap_add_handler=${__dhcp_debug_trap_add_handler/trap -- \'/}    # /- Strip `trap '...' SIGNAL` -> ...
+    __dhcp_debug_trap_add_handler=${__dhcp_debug_trap_add_handler%\'*}            # \-
+    __dhcp_debug_trap_add_handler=${__dhcp_debug_trap_add_handler//\'\\\'\'/\'}   # <- Unquote quoted quotes ('\'')
+    trap "${__dhcp_debug_trap_add_handler} $1;" "$2"
 }
-
 declare -f -t __dhcp_debug_trap__add
 
 
-#__setx_output
-
-#debug_prompt () { echo "[$BASH_SOURCE:$LINENO] $BASH_COMMAND?" _ ; BASH_COMMAND='echo b'; }
-#trap 'echo "<$_>"' DEBUG
-#trap-add 'echo "Bye bye"' EXIT
-#trap_add 'echo "in trap DEBUG"' DEBUG
-# dump set -x data to a file
-# turns on with a filename as $1
-# turns off with no params
-__setx_output "${OUTNAME}"
-exec &> >(ts '%Y-%m-%dT%H:%M:%S' | tee -a log.out)
-
-echo 'Hello from b'
-
-echo 'Hello again from b'
-
-
+if [[ -n "${DHCP_DEBUG_LOG_DIR}" ]]; then
+    mkdir -p "${DHCP_DEBUG_LOG_DIR}" || exit 1
+    __dhcp_debug_log_stdout "${DHCP_DEBUG_LOG_DIR}/stdout.log"
+    __dhcp_debug_log_stderr "${DHCP_DEBUG_LOG_DIR}/stderr.log"
+    __dhcp_debug_setx_output "${DHCP_DEBUG_LOG_DIR}/setx.log"
+fi
--- src/build-src/compilervars.sh
+++ src/build-src/compilervars.sh
@@ -29,10 +29,6 @@ export CMAKE_INSTALL_PREFIX="${CMAKE_INSTALL_PREFIX:-${DHCP_PREFIX}}"
 export CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}"
 export CUDAARCHS="${CUDAARCHS:-75;86}"
 
-export NCPU="${NCPU:-$(nproc)}"
-export CMAKE_BUILD_PARALLEL_LEVEL="${CMAKE_BUILD_PARALLEL_LEVEL:-${NCPU}}"
-export MAKEFLAGS="-j${CMAKE_BUILD_PARALLEL_LEVEL}"
-
 if [ -d "/usr/local/cuda/bin" ]; then
 	export CUDA_TOOLKIT_ROOT=/usr/local/cuda-12
 	export CUDACXX="${CUDA_TOOLKIT_ROOT}/bin/nvcc"
@@ -61,6 +57,9 @@ set_compiler_flags() {
 	export __INTEL_POST_CFLAGS="${__INTEL_POST_CFLAGS:+${__INTEL_POST_CFLAGS:-} }${2:-} ${INTEL_OPTIMIZER_FLAGS:-}"
 
 
+    export NCPU="${NCPU:-$(nproc)}"
+    export CMAKE_BUILD_PARALLEL_LEVEL="${CMAKE_BUILD_PARALLEL_LEVEL:-${NCPU}}"
+    
 	echo "INFO: Using ${NCPU} cpus with CMAKE_BUILD_PARALLEL_LEVEL=${CMAKE_BUILD_PARALLEL_LEVEL}" >&2
 	echo "INFO: Using __INTEL_PRE_CFLAGS=\"${__INTEL_PRE_CFLAGS:-}\"" >&2
 	echo "INFO: Using __INTEL_POST_CFLAGS=\"${__INTEL_POST_CFLAGS:-}\"" >&2
